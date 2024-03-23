580e18786f12cddcf52e3b0326dfcf81c52e3c36
NILESH GULE <vn_nilesh@yahoo.com>
2024-03-23 17:49:30 +0800

    integrated Spectre Console to improve console input output

--- how-would-they-react-dotnet/Program.cs
+++ how-would-they-react-dotnet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OllamaSharp;
+using Spectre.Console;
 
 var uri = new Uri("http://localhost:11434");
 var ollama = new OllamaApiClient(uri);
@@ -6,46 +7,47 @@ var ollama = new OllamaApiClient(uri);
 // select a model which should be used for further operations
 ollama.SelectedModel = "llama2";
 
-Console.WriteLine("Which famous person would you like to impersonate today?");
-var celebrityName = Console.ReadLine();
+var celebrityName = AnsiConsole.Ask<string>("Which famous person would you like to impersonate today? : ");
 
 Console.WriteLine();
-Console.WriteLine($"How would {celebrityName} reply to the following Tweet?");
+AnsiConsole.Markup($"How would [bold underline blue]{celebrityName}[/] reply to the following [bold underline blue]Tweet[/]?{Environment.NewLine}");
 var tweet = Console.ReadLine();
 
 ConversationContext context = null;
-context = await ollama.StreamCompletion($"How would {celebrityName} reply to the following Tweet which says {tweet} ?", context, stream => Console.Write(stream.Response));
+context = await ollama.StreamCompletion($"How would {celebrityName} reply to the following Tweet which says {tweet} ?", context, stream => AnsiConsole.Markup($"[green]{stream.Response.EscapeMarkup()}[/]"));
 
 Console.WriteLine();
 Console.WriteLine();
 
-Console.WriteLine("Would you like to continue the conversation and try some other celebrity? : Yes (Y) / No (N)");
-var continueConversation = Console.ReadLine();
 
 var moods = new List<string> { "happy", "sad", "angry", "excited", "bored", "confused", "surprised", "scared", "disgusted", "neutral" };
 Random random = new Random();
 
-while (continueConversation.ToLower() == "yes" || continueConversation.ToLower() == "y") 
-{
+bool continueConversation;
+
+do {
+    
+    continueConversation = AnsiConsole.Confirm("Would you like to continue the conversation and try some other celebrity?");
+    
     Console.WriteLine();
+    
+    if (continueConversation)
+    {
+        celebrityName = AnsiConsole.Ask<string>("Which famous person would you like to impersonate next? : ");
 
-    Console.WriteLine("Which famous person would you like to impersonate next?");
-    celebrityName = Console.ReadLine();
+        int randomIndex = random.Next(moods.Count);
+        string selectedMood = moods[randomIndex];
 
-    int randomIndex = random.Next(moods.Count);
-    string selectedMood = moods[randomIndex];
+        Console.WriteLine();
+        AnsiConsole.Markup($"This is how [bold underline blue]{celebrityName}[/] would react in [bold underline red]{selectedMood}[/] mood.");
 
-    Console.WriteLine();
-    Console.WriteLine($"This is how {celebrityName} would react in {selectedMood} mood.");
+        context = await ollama.StreamCompletion($"How would {celebrityName} reply to the following Tweet which says {tweet} in {selectedMood} mood?", context, stream => AnsiConsole.Markup($"[green]{stream.Response.EscapeMarkup()}[/]"));
 
-    context = await ollama.StreamCompletion($"How would {celebrityName} reply to the following Tweet which says {tweet} in {selectedMood} mood?", context, stream => Console.Write(stream.Response));
-
-    Console.WriteLine();
-    Console.WriteLine();
-
-    Console.WriteLine("Would you like to continue the conversation and try some other celebrity? : Yes (Y) / No (N)");
-    continueConversation = Console.ReadLine();
-}
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+    
+} while (continueConversation); 
 
 Console.WriteLine("Goodbye! But wait ...");
 context = await ollama.StreamCompletion("Say Goodbye in a very funny way based on current time of the day", context, stream => Console.Write(stream.Response));
